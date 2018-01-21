b377252904cbc6ca75d34c3f0303531626fcc236
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-01-22 00:03:25 +0100

    Add desired specs from String#succ

--- spec/tanga_spec.exs
+++ spec/tanga_spec.exs
@@ -13,5 +13,17 @@ defmodule TangaSpec do
     it "returns '<<koala>>' for '<<koalb>>'" do
       expect(Tanga.next("<<koala>>")).to eq("<<koalb>>")
     end
+    
+    xit "returns '2000aaa' for '1999zzz'" do
+      expect(Tanga.next("1999zzz")).to eq("2000aaa")
+    end
+    
+    xit "returns 'ZZZ9999' for 'AAAA0000'" do
+      expect(Tanga.next("ZZZ9999")).to eq("AAAA0000")
+    end
+    
+    xit "returns '**+' for '***'" do
+      expect(Tanga.next("***")).to eq("**+")
+    end
   end
 end
