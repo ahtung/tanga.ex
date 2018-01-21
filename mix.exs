209edba8cbf4b57b39fdbc0d4d995d31dbf445e9
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-01-21 23:41:21 +0100

    Add travis.yml and set min ex version to 1.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tanga.Mixfile do
     [
       app: :tanga,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.3",
       start_permanent: Mix.env == :prod,
       deps: deps(),
       preferred_cli_env: [espec: :test],
